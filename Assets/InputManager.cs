e95930e31db6fc6bd20bf2137faf86f861e2e758
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-07-31 21:50:07 +1200

    doc: removed commented code and debug logs

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -21,7 +21,6 @@ public class InputManager : MonoBehaviour
     }
     void Start()
     {
-        //inputActions.Mobile.TouchPress.started += context => TouchStart(context);
         inputActions.Mobile.TouchPos.started += context => TouchStart(context);
         inputActions.Mobile.TouchPress.canceled += context => TouchEnd(context);
         inputActions.Mobile.Drag.started += context => OnDrag(context);
@@ -31,9 +30,7 @@ public class InputManager : MonoBehaviour
     private void TouchStart(InputAction.CallbackContext context)
     {
         {
-            // PlayerController.i.startPos = Vector3.zero;
             Vector2 mousePos = inputActions.Mobile.TouchPos.ReadValue<Vector2>();
-            Debug.Log("Mouse Pos: " + mousePos);
             Ray ray = Camera.main.ScreenPointToRay(mousePos);
 
             if (Physics.Raycast(ray, out RaycastHit hit))
@@ -41,7 +38,6 @@ public class InputManager : MonoBehaviour
                 if (hit.collider.gameObject.name == "GolfBall")
                 {
                     PlayerController.i.startPos = new(hit.collider.gameObject.transform.position.x, 0, hit.collider.gameObject.transform.position.z);
-                    Debug.Log("Start Pos 1: " + PlayerController.i.startPos);
                     PlayerController.i.line.enabled = true;
                     PlayerController.i.line.positionCount = 1;
                     PlayerController.i.line.SetPosition(0, PlayerController.i.startPos);
@@ -50,15 +46,13 @@ public class InputManager : MonoBehaviour
                 else
                 {
                     {
-                        Debug.Log("Start Pos: 2");
                         GameObject p = GameObject.Find("GolfBall");
                         PlayerController.i.startPos = new(p.transform.position.x, 0, p.transform.position.z);
                         PlayerController.i.line.enabled = true;
                         PlayerController.i.line.positionCount = 2;
                         PlayerController.i.line.SetPosition(0, PlayerController.i.startPos);
                         PlayerController.i.endPos = new(hit.point.x, 0, hit.point.z);
-                        Debug.Log(" End Pos: " + PlayerController.i.startPos + " ," + new Vector3(hit.point.x, 0, hit.point.z));
-                        PlayerController.i.line.SetPosition(1,new(hit.point.x, 0, hit.point.z));
+                        PlayerController.i.line.SetPosition(1, new(hit.point.x, 0, hit.point.z));
                     }
                 }
 
@@ -74,7 +68,6 @@ public class InputManager : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(mousePos);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            Debug.Log("Hit: " + hit.collider.gameObject.name);
             return new(hit.point.x, 0, hit.point.z);
         }
         return Vector3.zero;
@@ -90,11 +83,9 @@ public class InputManager : MonoBehaviour
         {
             return;
         }
-        Debug.Log("Currently Moving At Pos " + ScreenToWorld(inputActions.Mobile.TouchPos.ReadValue<Vector2>()) + " From " + PlayerController.i.startPos + " which is a delta of " + PositionDifference(PlayerController.i.startPos, ScreenToWorld(inputActions.Mobile.TouchPos.ReadValue<Vector2>())));
         PlayerController.i.line.positionCount = 2;
         PlayerController.i.line.SetPosition(1, ScreenToWorld(inputActions.Mobile.TouchPos.ReadValue<Vector2>()));
         PlayerController.i.endPos = ScreenToWorld(inputActions.Mobile.TouchPos.ReadValue<Vector2>());
-
 
     }
     private void TouchEnd(InputAction.CallbackContext context)
@@ -103,21 +94,14 @@ public class InputManager : MonoBehaviour
         {
             return;
         }
-        // Ray ray = Camera.main.ScreenPointToRay(inputActions.Mobile.TouchPos.ReadValue<Vector2>());
-        // if (Physics.Raycast(ray, out RaycastHit hit))
-        // {
-        //     {
-        //         PlayerController.i.endPos = new(hit.point.x, 0, hit.point.z);
-        // Debug.Log("End Pos: " + PlayerController.i.endPos);
+
         if (PlayerController.i.startPos != PlayerController.i.endPos && PlayerController.i.startPos != Vector3.zero)
         {
 
             PlayerController.i.line.enabled = false;
-            Debug.Log("Apply Force" + PositionDifference(PlayerController.i.startPos, PlayerController.i.endPos));
             PlayerController.i.Launch(PositionDifference(PlayerController.i.startPos, PlayerController.i.endPos));
         }
-        //     }
-        // }
+
 
 
     }
