9e693067e50ddc46b0e2025d3bedb75ff01e5d3b
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-07-31 21:51:15 +1200

    add: line changes color depending on length

---
+++ Assets/ProjectionLine.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectionLine : MonoBehaviour
+{
+    public Gradient fullGradient;
+    public Gradient mediumGradient;
+    public Gradient lowGradient;
+
+    public const float FULL_THRESHOLD = 2.5f;
+    public const float MEDIUM_THRESHOLD = 1.5f;
+
+    void Update()
+    {
+        if(PlayerController.i.startPos != Vector3.zero && PlayerController.i.endPos != Vector3.zero)
+        {
+            float distance = Vector3.Distance(PlayerController.i.startPos, PlayerController.i.endPos);
+            if(distance >= FULL_THRESHOLD)
+            {
+                PlayerController.i.line.colorGradient = fullGradient;
+            }
+            else if(distance >= MEDIUM_THRESHOLD)
+            {
+                PlayerController.i.line.colorGradient = mediumGradient;
+            }
+            else
+            {
+                PlayerController.i.line.colorGradient = lowGradient;
+            }
+        }
+    }
+}
