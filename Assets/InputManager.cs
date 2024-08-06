a6ae8aec87b0cb8333b5db360fcfc2839c4861a7
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-08-07 09:38:38 +1200

    fix: ball was not being moved

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -4,7 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class InputManager : MonoBehaviour
 {
-    [SerializeField] private LayerMask ignoreLayer;
     private GolfGameInputActions inputActions;
 
     void Awake()
@@ -33,6 +32,7 @@ public class InputManager : MonoBehaviour
     private void TouchStart(InputAction.CallbackContext context)
     {
         {
+            Debug.Log("TouchStart ");
             Vector2 mousePos = inputActions.Mobile.TouchPos.ReadValue<Vector2>();
             Rigidbody rb = GameObject.Find("GolfBall").GetComponent<Rigidbody>();
             rb.velocity = Vector3.zero;
@@ -42,6 +42,7 @@ public class InputManager : MonoBehaviour
             {
                 if (hit.collider.gameObject.name == "GolfBall")
                 {
+                    Debug.Log("GolfBall hit");
                     PlayerController.i.startPos = new(
                         hit.collider.gameObject.transform.position.x,
                         0,
@@ -55,15 +56,18 @@ public class InputManager : MonoBehaviour
                 else if(hit.collider.gameObject.tag == "Floor")
                     {
                         GameObject p = GameObject.Find("GolfBall");
+                        Debug.Log("p " + p.transform.position);
                         PlayerController.i.startPos = new(
                             p.transform.position.x,
                             0,
                             p.transform.position.z
                         );
+                        Debug.Log("Floor hit " + PlayerController.i.startPos);
                         PlayerController.i.line.enabled = true;
                         PlayerController.i.line.positionCount = 2;
                         PlayerController.i.line.SetPosition(0, PlayerController.i.startPos);
                         PlayerController.i.endPos = new(hit.point.x, 0, hit.point.z);
+                        Debug.Log("EndPos " + PlayerController.i.endPos + " " + hit.point + " StartPos " + PlayerController.i.startPos);
                         PlayerController.i.line.SetPosition(1, new(hit.point.x, 0, hit.point.z));
                     }
                 
@@ -78,24 +82,34 @@ public class InputManager : MonoBehaviour
 
     private Vector3 ScreenToWorld(Vector2 mousePos)
     {
+        Debug.Log("ScreenToWorld");
         Ray ray = Camera.main.ScreenPointToRay(mousePos);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             if (hit.collider.gameObject.tag == "Floor")
+            {
+                Debug.Log(hit.collider.gameObject.name + ", " + hit.collider.gameObject.tag);
                 return new(hit.point.x, 0, hit.point.z);
+            }
             else
+            {
+                Debug.Log(hit.collider.gameObject.name + ", " + hit.collider.gameObject.tag);
                 return Vector3.zero;
+
+            }
         }
         return Vector3.zero;
     }
 
     private Vector3 PositionDifference(Vector3 start, Vector3 end)
     {
+        Debug.Log("PositionDifference");
         return new(end.x - start.x, 0, end.z - start.z);
     }
 
     private void OnDrag(InputAction.CallbackContext context)
     {
+        Debug.Log("OnDrag");
         if (
             PlayerController.i.startPos == Vector3.zero
             || PlayerController.i.line.GetPosition(0) == Vector3.zero
@@ -116,13 +130,14 @@ public class InputManager : MonoBehaviour
 
     private void TouchEnd(InputAction.CallbackContext context)
     {
+        Debug.Log("TouchEnd");
 
         if (
             PlayerController.i.startPos == Vector3.zero
             || PlayerController.i.line.GetPosition(0) == Vector3.zero
         )
         {
-
+            Debug.Log("Return InputManager:125");
 
             return;
         }
@@ -133,10 +148,11 @@ public class InputManager : MonoBehaviour
         )
         {
 
+            Debug.Log("Launch InputManager:134");
             PlayerController.i.Launch(
                 PositionDifference(PlayerController.i.startPos, PlayerController.i.endPos)
             );
-            PlayerController.i.startPos = PlayerController.i.endPos;
+            //PlayerController.i.startPos = PlayerController.i.endPos;
             PlayerController.i.line.SetPosition(1, PlayerController.i.endPos);
             PlayerController.i.line.SetPosition(0, PlayerController.i.endPos);
         }
