990c197bc06ccf3aa16f977c14f888420275ac4a
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2024-09-04 09:59:57 +1200

    add: life system working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,12 +20,16 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
 
-    public Vector3 startPos; //The starting position of the swipe
-    public Vector3 endPos; //The ending position of the swipe
+    [HideInInspector] public Vector3 startPos; //The starting position of the swipe
+    [HideInInspector] public Vector3 endPos; //The ending position of the swipe
+    [HideInInspector] public Vector3 RespawnPosition; //The direction of the swipe
     public float speedModifier;
     public static PlayerController i;
     public LineRenderer line;
     public int sectionMoves = 0;
+    public int LivesRemaining;
+    [SerializeField] private bool respawning = false;
+    [SerializeField] private List<GameObject> lifeIcons = new List<GameObject>();
 
 
 
@@ -47,6 +51,8 @@ public class PlayerController : MonoBehaviour
         gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
         startPos = Vector3.zero;
         endPos = Vector3.zero;
+        RespawnPosition = gameObject.transform.position;
+        LivesRemaining = 3;
 
     }
 
@@ -55,6 +61,31 @@ public class PlayerController : MonoBehaviour
         Vector3 direction = new Vector3(-delta.x, 0, -delta.z);
         sectionMoves++;
         gameObject.GetComponent<Rigidbody>().AddForce(direction * speedModifier, ForceMode.Impulse);
+        
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("OutOfBounds"))
+        {
+            gameObject.transform.position = RespawnPosition;
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            if (LivesRemaining > 0)
+            {
+                if (!respawning)
+                {
+                    lifeIcons[LivesRemaining - 1].SetActive(false);
+                    LivesRemaining--;
+                    Debug.Log("Player has " + LivesRemaining + " lives remaining");
+                    return;
+                }
+            }
+            else
+            {
+                Debug.Log("Player has no lives remaining");
+            }
+            respawning = false;
+        }
         
     }
 
